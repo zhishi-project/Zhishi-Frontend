01e720d92082f95484fa89f39c8470e1df49ea61
Innocent <innocent.amadi@andela.com>
2016-02-05 14:32:23 +0100

    add deploy script to package.json

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 var node_modules_dir = path.resolve(__dirname, 'node_modules');
 
 var config = {
-  entry: path.resolve(__dirname, 'app/app.js'),
+  entry: path.resolve(__dirname, 'app/js/app.js'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
