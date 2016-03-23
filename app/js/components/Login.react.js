d0c5d8f7bc06db2d350f7eaed36e3a98d1b3d49d
Innocent <innocent.amadi@andela.com>
2016-03-23 15:11:43 +0100

    implements homepage slider

--- app/js/components/Login.react.js
+++ app/js/components/Login.react.js
@@ -1,21 +1,57 @@
 import React from 'react'
 import webAPI from '../utils/webAPI.js'
 import AuthStore from '../stores/AuthStore.js'
+import DisplayStore from '../stores/DisplayStore.js'
 import AuthActions from '../actions/AuthActions.js'
 import Config from "../config/environment.js"
 
+
 let signUpPath = `${Config.host}/login/google?redirect_url=http://${window.parent.location.host}/login/auth`;
+
+let loginState = () => {
+  return { quotes: DisplayStore.getQuotes() }
+}
 
 class Login extends React.Component {
   constructor(props, context) {
     super(props);
+    this.state = loginState();
   }
 
   signIn() {
     let path = '/login/google?redirect_url=http://localhost:8080/login/auth';
     window.location.href = signUpPath;
   }
-  
+
+  componentDidMount() {
+    var chosenQuotes = function(quotes) {
+      return shuffle(quotes)
+    }
+
+    function shuffle(a) {
+        var j, x, i;
+        for (i = a.length; i; i -= 1) {
+            j = Math.floor(Math.random() * i);
+            x = a[i - 1];
+            a[i - 1] = a[j];
+            a[j] = x;
+        }
+        return a;
+    }
+
+    $(".messages").typeIt({
+      strings: chosenQuotes(this.state.quotes),
+      speed: 90,
+      startDelay: 1000,
+      breakDelay: 3000,
+      cursor: true,
+      breakLines: false,
+      loop: true,
+      lifeLike: false,
+      loopDelay: 500
+    });
+  }
+
   render(){
     return (
       <div className="index center aligned ui container full-height">
@@ -27,7 +63,7 @@ class Login extends React.Component {
           </header>
 
           <div className="sign-in-area text container">
-            <h1>A problem shared is a problem half-solved!</h1>
+            <h1 className="messages"></h1>
             <a href={signUpPath}  className="ui button">
               <i className="google plus icon"></i>
               Sign in with your Andela email
