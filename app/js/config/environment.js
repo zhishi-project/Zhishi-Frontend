a9f0d72672fc38002842979a1d9cf6fe1ca04437
Daniel Chinedu <chinedudaniel7@gmail.com>
2016-04-10 18:33:04 +0100

    feat (Tag selection modal): Implement modal for tag selection
    Implement modal for tag selection:
    - Create component for modal
    - Initialize modal on page load
    - Add basic information to tags
    - Make modal non-closable

--- app/js/config/environment.js
+++ app/js/config/environment.js
@@ -1,6 +1,6 @@
 module.exports = {
-  host: "http://zhishi-engine.herokuapp.com",
-  //host: "http://zhishi-test.herokuapp.com",
+  // host: "http://zhishi-engine.herokuapp.com",
+  host: "http://zhishi-test.herokuapp.com",
   slackZhishiChannel: "https://hooks.slack.com/services/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",
   slackHookName: "zhishi"
 }
