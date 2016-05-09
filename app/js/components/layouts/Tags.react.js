99a5110eed37272ac2ede05f0c8a23c3d0a9ca7e
Innocent <innocent.amadi@andela.com>
2016-05-09 22:39:02 +0100

    further refractors the tags component

--- app/js/components/layouts/Tags.react.js
+++ app/js/components/layouts/Tags.react.js
@@ -5,22 +5,15 @@ const Tag = ({ tag }) => {
   return <span>{tag}</span>
 }
 
-class Tags extends React.Component {
-  constructor(props, context){
-    super(props)
-   }
-
-   render () {
-     const { tags } = this.props;
-     let tags_array = [];
-     if (!$.isEmptyObject(tags)) {
-       for(var i = 0; i < tags.length; i++) {
-         tags_array.push(<Tag key={i} tag={tags[i]} />)
-       }
+const Tags = ({ tags }) => {
+   let tags_array = [];
+   if (!$.isEmptyObject(tags)) {
+     for(var i = 0; i < tags.length; i++) {
+       tags_array.push(<Tag key={i} tag={tags[i]} />)
      }
-     return (
-       <div className="tags">{tags_array}</div>
-     )
    }
+   return (
+     <div className="tags">{tags_array}</div>
+   )
  }
 export default Tags;
