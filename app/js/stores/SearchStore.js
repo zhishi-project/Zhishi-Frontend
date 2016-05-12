eb5daca53f3e184321c2849c0b5c5e7ed4ed1996
Gertrude <gertrude@Gertrudes-MacBook-Pro.local>
2016-05-12 15:37:55 +0300

    Refactors User and Search Stores

--- app/js/stores/SearchStore.js
+++ app/js/stores/SearchStore.js
@@ -1,49 +1,81 @@
-var AppDispatcher = require('../dispatcher/AppDispatcher');
-var EventEmitter = require('events').EventEmitter;
-var ZhishiConstants = require('../constants/ZhishiConstants');
-var assign = require('object-assign');
-import Common from '../utils/Common.js'
+import AppDispatcher from '../dispatcher/AppDispatcher';
+import {EventEmitter} from 'events';
+import ZhishiConstants from '../constants/ZhishiConstants';
+import assign from 'object-assign';
+import Common from '../utils/Common.js';
+import BaseStore from './BaseStore';
 
-var CHANGE_EVENT = 'change';
+class SearchStore extends BaseStore {
+  constructor() {
+    super();
+    this.search_results = [];
+  }
 
-var search_results = [];
+  loadSearchResult(results) {
+    this.search_results = results.reverse();
+  }
 
-let loadSearchResults = (results) => {
-  search_results = results.reverse();
+  getSearchResults() {
+    return this.search_results;
+  }
+
+  _registerActions(action) {
+    switch (action.actionType) {
+      case ZhishiConstants.RECEIVE_SEARCH_RESULTS:
+        if (action.data) {
+          loadSearchResults(action.data.questions);
+        }
+        SearchStore.emitChange();
+        break;
+
+      default:
+        // Nothing for now
+    }
+  }
 }
 
-var SearchStore = assign({}, EventEmitter.prototype, {
+export default new SearchStore();
 
-  getSearchResults: function() {
-    return search_results;
-  },
-
-  emitChange: function() {
-    this.emit(CHANGE_EVENT);
-  },
-
-  addChangeListener: function(callback) {
-    this.on(CHANGE_EVENT, callback);
-  },
-
-  removeChangeListener: function(callback) {
-    this.removeListener(CHANGE_EVENT, callback);
-  }
-
-})
-
-SearchStore.dispatchToken = AppDispatcher.register(function(action) {
-
-  switch (action.actionType) {
-    case ZhishiConstants.RECEIVE_SEARCH_RESULTS:
-      if (action.data) {
-        loadSearchResults(action.data.questions);
-      }
-      SearchStore.emitChange();
-      break;
-    default:
-
-  }
-})
-
-export default SearchStore;
+// var CHANGE_EVENT = 'change';
+//
+// var search_results = [];
+//
+// let loadSearchResults = (results) => {
+//   search_results = results.reverse();
+// }
+//
+// var SearchStore = assign({}, EventEmitter.prototype, {
+//
+//   getSearchResults: function() {
+//     return search_results;
+//   },
+//
+//   emitChange: function() {
+//     this.emit(CHANGE_EVENT);
+//   },
+//
+//   addChangeListener: function(callback) {
+//     this.on(CHANGE_EVENT, callback);
+//   },
+//
+//   removeChangeListener: function(callback) {
+//     this.removeListener(CHANGE_EVENT, callback);
+//   }
+//
+// })
+//
+// SearchStore.dispatchToken = AppDispatcher.register(function(action) {
+//
+//   switch (action.actionType) {
+    // case ZhishiConstants.RECEIVE_SEARCH_RESULTS:
+    //   if (action.data) {
+    //     loadSearchResults(action.data.questions);
+    //   }
+    //   SearchStore.emitChange();
+    //   break;
+//     default:
+//
+//   }
+// })
+//
+// export default SearchStore;
--- app/js/stores/UserStore.js
+++ app/js/stores/UserStore.js
@@ -1,93 +1,160 @@
 import AppDispatcher from '../dispatcher/AppDispatcher';
-import { EventEmitter } from 'events'
+import { EventEmitter } from 'events';
 import ZhishiConstants from '../constants/ZhishiConstants';
 import assign from 'object-assign';
-import CVar from "../config/CookieVariables.js"
-import AuthStore from "./AuthStore.js"
+import CVar from '../config/CookieVariables.js';
+import BaseStore from './BaseStore';
+import AuthStore from './AuthStore.js';
 
-let CHANGE_EVENT = 'change';
+class UserStore extends BaseStore {
+  constructor() {
+    super();
+    this._users = {};
+    this._currentUser = {};
+  }
 
-let _users = {}, _current_user = {};
+  update(id, updates) {
+    this._users[id] = assign({}, this._users[id], updates);
+  }
 
+  updateCurrentUser(user) {
+    user = (typeof user === 'object') ? JSON.stringify(user) : user;
+    $.cookie(CVar.this.currentUser, user || {});
+    update(user.id, user);
+  }
 
+  destroy(id) {
+    delete _user[id];
+  }
 
-function update(id, updates) {
-  _users[id] = assign({}, _users[id], updates);
-}
+  getAllUsers() {
+    return this._users;
+  }
 
-function updateCurrentUser(user){
-  // user = (typeof user === 'object') ? JSON.stringify(user) : user
-  // $.cookie(CVar.current_user, user || {});
-  // update(user.id, user);
-}
+  getUser(id) {
+    return this._users[id];
+  }
 
-
-/**
- * Delete a User from the store
- */
-function destroy(id) {
-  delete _user[id];
-}
-
-
-var UserStore = assign({}, EventEmitter.prototype, {
-
-  getAllUsers: function() {
-    return _users;
-  },
-
-  getUser: function(id) {
-    return _users[id];
-  },
-
-  getCurrentUser: function() {
+  getCurrentUser() {
     return AuthStore.getCurrentUser();
-  },
-
-  getFullName: function(user) {
-    return (user.first_name || "") + " " + (user.last_name || "");
-  },
-
-  emitChange: function() {
-    this.emit(CHANGE_EVENT);
-  },
-
-  addChangeListener: function(callback) {
-    this.on(CHANGE_EVENT, callback);
-  },
-
-  removeChangeListener: function(callback) {
-    this.removeListener(CHANGE_EVENT, callback);
   }
-});
 
-// Register callback to handle all updates
-UserStore.dispatchToken = AppDispatcher.register(function(action) {
-  var text;
-
-  switch(action.actionType) {
-
-
-
-    case ZhishiConstants.AUTH_LOG_OUT:
-      if (AppDispatcher._isDispatching) { AppDispatcher.waitFor([AuthStore.dispatchToken]) };
-      UserStore.emitChange();
-      break;
-
-    case ZhishiConstants.RECEIVE_USER:
-      if (action.data)
-      update(action.data.id, action.data)
-      UserStore.emitChange();
-      break;
-
-    case ZhishiConstants.AUTH_LOG_OUT:
-      clearUsers();
-      UserStore.emitChange();
-      break;
-
-    default:
-      // nothing for now
+  getFullName(user) {
+    return (user.first_name || '') + (user.last_name || '');
   }
-});
 
-export default UserStore;
+  _registerActions(action) {
+    switch (action.actionType) {
+      case ZhishiConstants.AUTH_LOG_OUT:
+        if (AppDispatcher._isDispatching) {
+          AppDispatcher.waitFor([AuthStore.dispatchToken])
+        };
+          UserStore.emitChange();
+        break;
+
+      case ZhishiConstants.RECEIVE_USER:
+        if (action.data) {
+          update(action.data.id, action.data)
+          UserStore.emitChange();
+        }
+        break;
+
+      case ZhishiConstants.AUTH_LOG_OUT:
+        clearUsers();
+        UserStore.emitChange();
+        break;
+
+      default:
+          // Nothing for now
+      }
+    }
+  }
+
+  export default new UserStore();
+
+// let CHANGE_EVENT = 'change';
+//
+// let _users = {}, _current_user = {};
+//
+//
+//
+// function update(id, updates) {
+//   _users[id] = assign({}, _users[id], updates);
+// }
+//
+// function updateCurrentUser(user){
+//   // user = (typeof user === 'object') ? JSON.stringify(user) : user
+//   // $.cookie(CVar.current_user, user || {});
+//   // update(user.id, user);
+// }
+//
+//
+// /**
+//  * Delete a User from the store
+//  */
+// function destroy(id) {
+//   delete _user[id];
+// }
+//
+//
+// var UserStore = assign({}, EventEmitter.prototype, {
+//
+//   getAllUsers: function() {
+//     return _users;
+//   },
+//
+//   getUser: function(id) {
+//     return _users[id];
+//   },
+//
+//   getCurrentUser: function() {
+//     return AuthStore.getCurrentUser();
+//   },
+//
+//   getFullName: function(user) {
+//     return (user.first_name || "") + " " + (user.last_name || "");
+//   },
+//
+//   emitChange: function() {
+//     this.emit(CHANGE_EVENT);
+//   },
+//
+//   addChangeListener: function(callback) {
+//     this.on(CHANGE_EVENT, callback);
+//   },
+//
+//   removeChangeListener: function(callback) {
+//     this.removeListener(CHANGE_EVENT, callback);
+//   }
+// });
+//
+// // Register callback to handle all updates
+// UserStore.dispatchToken = AppDispatcher.register(function(action) {
+//   var text;
+//
+//   switch(action.actionType) {
+//
+//
+//
+    // case ZhishiConstants.AUTH_LOG_OUT:
+    //   if (AppDispatcher._isDispatching) { AppDispatcher.waitFor([AuthStore.dispatchToken]) };
+    //   UserStore.emitChange();
+    //   break;
+//
+    // case ZhishiConstants.RECEIVE_USER:
+    //   if (action.data)
+    //   update(action.data.id, action.data)
+    //   UserStore.emitChange();
+    //   break;
+//
+  //   case ZhishiConstants.AUTH_LOG_OUT:
+  //     clearUsers();
+  //     UserStore.emitChange();
+  //     break;
+  //
+  //   default:
+  //     // nothing for now
+  // }
+// });
+//
+// export default UserStore;
