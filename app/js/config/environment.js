e543aae45780e3dd2c0c89a9b274a985984bd690
Innocent <innocent.amadi@andela.com>
2016-05-29 03:10:35 +0100

    switch from test

--- app/js/config/environment.js
+++ app/js/config/environment.js
@@ -1,6 +1,6 @@
 module.exports = {
-  // host: "//zhishi-engine.herokuapp.com",
-  host: "//zhishi-test.herokuapp.com",
+  host: "//zhishi-engine.herokuapp.com",
+  // host: "//zhishi-test.herokuapp.com",
   slackZhishiChannel: "https://hooks.slack.com/services/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",
   slackHookName: "zhishi_feedback"
 }
