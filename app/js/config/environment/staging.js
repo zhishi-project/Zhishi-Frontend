7d61ba0ee94f29d95b57c48695b0a771a029a126
Emmanuel Chigbo <emmanuel.chigbo@andela.com>
2016-10-29 13:58:00 +0100

    Remove the hardcoded protocol the zhishiPermanentSite variable

--- app/js/config/environment/staging.js
+++ app/js/config/environment/staging.js
@@ -1,5 +1,5 @@
 module.exports = {
   slackZhishiChannel: 'https://hooks.slack.com/services/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',
   slackHookName: 'zhishi_feedback',
-  zhishiPermanentSite: 'https://zhishi-test.andela.com'
+  zhishiPermanentSite: '//zhishi-test.andela.com'
 };
