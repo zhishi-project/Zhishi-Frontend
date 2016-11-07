b0ac7a07d2eb427c5c80f277422649bfab007c93
Innocent <innocent.amadi@andela.com>
2016-11-07 06:27:47 -0800

    implement a markdown enabled textaea

--- app/js/components/questions/QuestionContent.react.js
+++ app/js/components/questions/QuestionContent.react.js
@@ -2,19 +2,18 @@ import React, {PropTypes} from 'react'; // eslint-disable-line no-unused-vars
 import TinyMCE from 'react-tinymce';
 import tinymceConfig from '../../config/tinymceConfig.js';
 import Common from '../../utils/Common.js';
+import ReactMarkdown from 'react-markdown';
 
 const QuestionContent = ({question, onChange}) => {
   return question.editing ?
-       <TinyMCE
+       <textarea
          content={question.content}
-         config={tinymceConfig.forContent(`#questionContent-${question.id}`)}
+         value={question.content}
          className="content"
-         data-id="content"
-         onChange={onChange}
-         value="" /> :
-       <div dangerouslySetInnerHTML={{
-         __html: Common.replaceAtMentionsWithLinks(question.content)
-       }} />;
+         onChange={onChange} /> :
+        <ReactMarkdown 
+          source={question.content}
+          htmlMode='raw' />;
 };
 
 QuestionContent.propTypes = {
