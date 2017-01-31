b5860c89eae3270cfe1b2fb320ab9d52d0d583c6
andela-akolawole <aladekolawole15@yahoo.com>
2017-01-31 09:00:08 +0100

    feat(slack-notifications): Add toggleSlackNotification - Add toggleSlackNoficiations [Finshes #137598387]

--- app/js/actions/UserActions.js
+++ app/js/actions/UserActions.js
@@ -1,3 +1,4 @@
+import fetch from 'isomorphic-fetch';
 import types from '../constants/users/actionTypes';
 import webAPI from './../utils/webAPI.js';
 
@@ -16,4 +17,25 @@ export function loadUser(userId) {
         dispatch(loadUserSuccess(user));
       });
   };
+}
+
+/**
+ * toggleSlackNotifications
+ * @desc This changes the status of the slack notification
+ * @param {Object} currentUser - The current user details
+ * @param {Boolean} status - The could be true/false
+ * @return {Func} callback promise
+ */
+export function toggleSlackNotifications(currentUser, status) {
+  console.log(process.env.ZI_NOTIFI_URL, 'notify url');
+  const body = JSON.stringify({status});
+  return fetch({
+    url: `/users/${currentUser.id}/preference`,
+    baseUrl: process.env.ZI_NOTIFI_URL,
+    body,
+    method: 'POST',
+    headers: {
+      Authorization: `Token token=${currentUser.apiKey}`
+    }
+  });
 }
