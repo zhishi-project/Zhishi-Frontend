a6f120e5371fd84085e7d3d2c356a722fe12e101
andela-akolawole <aladekolawole15@yahoo.com>
2017-02-03 14:49:32 +0100

    feat(bugsnag-redux): Integrate a bugsnag in redux - Integrate redux-bugsnag-middleware into redux to monitor actions and reducers [Finishes #137791351]

--- app/js/stores/configureStore.js
+++ app/js/stores/configureStore.js
@@ -1,10 +1,13 @@
 import {createStore, applyMiddleware} from 'redux';
-import rootReducer from '../reducers';
+import bugsnagMiddleware from 'redux-bugsnag-middleware';
+import Bugsnag from 'bugsnag-js';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import thunk from 'redux-thunk';
+import rootReducer from '../reducers';
 
+Bugsnag.apiKey = process.env.BUGSNAG_API;
 const middleware = process.env.NODE_ENV === 'production' ?
-  [thunk] :
+  [bugsnagMiddleware(), thunk] :
   [reduxImmutableStateInvariant(), thunk];
 
 const configureStore = initialState => {
