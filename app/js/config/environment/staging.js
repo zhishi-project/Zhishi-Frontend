bfb0c1fc25a8d4e6368fb5f288eb06a249686764
oreoluwa <oreoluwa.akinniranye@andela.com>
2017-04-29 16:16:21 +0100

    bunch of fixes for frontend implemmentations

--- app/js/config/environment/staging.js
+++ app/js/config/environment/staging.js
@@ -12,10 +12,3 @@ module.exports = {
     }
   }
 };
-
-// module.exports = {
-//   auth_url: 'http://api-staging.andela.com/login',
-//   slackZhishiChannel: 'https://hooks.slack.com/services/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',
-//   slackHookName: 'zhishi_feedback',
-//   zhishiPermanentSite: '//zhishi-test.andela.com'
-// };
--- tools/srcServer.js
+++ tools/srcServer.js
@@ -25,7 +25,7 @@ app.listen(port, function(err) {
   if (err) {
     console.log(err);
   } else {
-    open(`http://localhost:${port}`)
+    open(`http://localhost:${port}`);
     console.log(`Server running on port: ${port}`);
   }
 });
