9aa19bf355a01c4b1612a559216658ec9995f19f
oreoluwa <oreoluwa.akinniranye@andela.com>
2017-04-29 16:13:59 +0100

    bunch of fixes for frontend implemmentations

--- app/js/config/environment/staging.js
+++ app/js/config/environment/staging.js
@@ -1,6 +1,21 @@
 module.exports = {
+  devHost: '//zhishi-dev.andela.com',
+  zhishiPermanentSite: '//zhishi-test.andela.com',
   auth_url: 'http://api-staging.andela.com/login',
-  slackZhishiChannel: 'https://hooks.slack.com/services/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',
-  slackHookName: 'zhishi_feedback',
-  zhishiPermanentSite: '//zhishi-test.andela.com'
+  slack: {
+    username: 'zhishi_dev',
+    url: 'https://hooks.slack.com/services/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',
+    channels: {
+      team: '#zhishi_q_and_a_dev',
+      feedback: '#zhishi_q_and_a_dev',
+      q_and_a: '#zhishi_q_and_a_dev',
+    }
+  }
 };
+
+// module.exports = {
+//   auth_url: 'http://api-staging.andela.com/login',
+//   slackZhishiChannel: 'https://hooks.slack.com/services/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',
+//   slackHookName: 'zhishi_feedback',
+//   zhishiPermanentSite: '//zhishi-test.andela.com'
+// };
